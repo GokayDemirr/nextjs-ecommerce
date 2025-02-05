fcc8e38b0db9c7292b8cb8658ee601692ddc712d
GokayDemirr <gokaydemirpc@gmail.com>
2025-02-05 15:52:45 +0300

    feat: Add data for carousel and navbar

---
+++ src/data/carouselImages.js
@@ -0,0 +1,14 @@
+export const carouselImages = [
+  {
+    description: "Modern, Premium, Versatile.",
+    src: "/images/carousel/slide1.jpg",
+    title: "Cintovibe Collection",
+    infoBoxPosition: "bottom-1/3 right-1/6",
+  },
+  {
+    description: "Exclusive Deals, Limited Time!",
+    src: "/images/carousel/slide2.jpg",
+    title: "Summer Sale",
+    infoBoxPosition: "top-1/3 left-1/6",
+  },
+];
---
+++ src/data/categories.js
@@ -0,0 +1,115 @@
+export const bagCategories = [
+  {
+    name: "Kadın Çantaları",
+    items: [
+      "El Çantaları",
+      "Sırt Çantaları",
+      "Omuz Çantaları",
+      "Kol Çantaları",
+      "Bel Çantaları",
+    ],
+  },
+  {
+    name: "Erkek Çantaları",
+    items: ["Sırt Çantaları", "Evrak Çantaları", "Bel Çantaları"],
+  },
+  {
+    name: "Seyahat Çantaları",
+    items: ["Valizler", "Seyahat Sırt Çantaları", "Kabin Boy Çantalar"],
+  },
+  {
+    name: "Spor Çantaları",
+    items: ["Yoga Çantaları", "Spor Çantaları"],
+  },
+  {
+    name: "Çanta Aksesuarları",
+    items: ["Çanta Askıları", "Çanta İç Organizatörleri", "Çanta Kılıfları"],
+  },
+];
+
+export const beltCategories = [
+  {
+    name: "Erkek Kemeri",
+    items: ["Deri Kemeler", "Tokalı Kemeler", "Casual Kemeler"],
+  },
+  {
+    name: "Kadın Kemeri",
+    items: ["Deri Kemeler", "İnce Kemeler", "İki Renkli Kemeler"],
+  },
+  {
+    name: "Spor Kemeri",
+    items: ["Elastik Kemeler", "Sporcu Kemeri"],
+  },
+  {
+    name: "Çocuk Kemeri",
+    items: ["Eğlenceli Tasarımlar", "Deri Kemeler"],
+  },
+];
+
+export const walletCategories = [
+  {
+    name: "Erkek Cüzdanları",
+    items: [
+      "Deri Cüzdanlar",
+      "Kartlıklar",
+      "Minimalist Cüzdanlar",
+      "Fermuarlı Cüzdanlar",
+    ],
+  },
+  {
+    name: "Kadın Cüzdanları",
+    items: ["Uzun Cüzdanlar", "Kısa Cüzdanlar", "Kartlıklar"],
+  },
+  {
+    name: "Cüzdan Aksesuarları",
+    items: ["Cüzdan Zinciri", "Cüzdan Çantası"],
+  },
+];
+
+export const watchCategories = [
+  {
+    name: "Erkek Saatleri",
+    items: [
+      "Kol Saatleri",
+      "Akıllı Saatler",
+      "Spor Saatleri",
+      "Deri Kayışlı Saatler",
+    ],
+  },
+  {
+    name: "Kadın Saatleri",
+    items: ["Takı Saatleri", "Analog Saatler", "Akıllı Saatler"],
+  },
+  {
+    name: "Unisex Saatler",
+    items: ["Minimalist Saatler", "Bilezikli Saatler", "Akıllı Saatler"],
+  },
+  {
+    name: "Saat Aksesuarları",
+    items: ["Saat Kayışları", "Saat Kutuları", "Saat Temizleme Setleri"],
+  },
+];
+
+export const glassesCategories = [
+  {
+    name: "Erkek Gözlükleri",
+    items: [
+      "Aviator Gözlükler",
+      "Yuvarlak Gözlükler",
+      "Kare Gözlükler",
+      "Spor Gözlükleri",
+    ],
+  },
+  {
+    name: "Kadın Gözlükleri",
+    items: ["Büyük Çerçeve Gözlükler", "Oval Gözlükler", "Güneş Gözlükleri"],
+  },
+  {
+    name: "Çocuk Gözlükleri",
+    items: ["Çocuk Güneş Gözlükleri", "Eğlenceli Tasarımlar"],
+  },
+  {
+    name: "Gözlük Aksesuarları",
+    items: ["Gözlük Kabı", "Gözlük Temizleme Setleri", "Lens Temizleme Spreyi"],
+  },
+];
