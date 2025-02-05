700057bdb775c029dbc9bc0d18ea9206ed204369
GokayDemirr <gokaydemirpc@gmail.com>
2025-02-05 15:56:20 +0300

    feat: Add Carousel component to Home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <div className="text-3xl">hey</div>;
-}
+import Carousel from "@/components/Hero/Carousel";
+
+const Home = () => {
+  return (
+    <>
+      <Carousel /> <div className="relative">hey</div>
+    </>
+  );
+};
+
+export default Home;
