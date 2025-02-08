f74470d70dc91d4c6c948f7084999c8332fd7c57
GokayDemirr <gokaydemirpc@gmail.com>
2025-02-08 23:08:42 +0300

    chore: Create category image data with grid span properties

---
+++ src/data/categoryImages.js
@@ -0,0 +1,24 @@
+export const categoryImages = [
+  {
+    src: "/images/categoryImages/female.webp",
+    name: "Women",
+    alt: "Women Category",
+    className: "row-span-2 col-span-2 ",
+  },
+  {
+    src: "/images/categoryImages/bag.avif",
+    name: "Bag",
+    alt: "Bag Category",
+  },
+  {
+    src: "/images/categoryImages/watch.webp",
+    name: "Watch",
+    className: "row-span-2 ",
+    alt: "Watch Category",
+  },
+  {
+    src: "/images/categoryImages/wallet.avif",
+    name: "Wallet",
+    alt: "Wallet Category",
+  },
+];
