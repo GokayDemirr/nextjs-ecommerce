a6c282b4a11b4cfe1ede570b67e26b2d28632405
GokayDemirr <gokaydemirpc@gmail.com>
2025-02-11 21:09:40 +0300

    Add TrendingProducts component

---
+++ src/components/TrendingProducts.jsx
@@ -0,0 +1,20 @@
+import ProductList from "./ProductList";
+
+const TrendingProducts = () => {
+  return (
+    <div className="max-w-screen-xl pt-20">
+      <div className="flex items-center justify-center ">
+        <div className="w-14 h-[2px] bg-black rounded-lg"></div>
+        <div className="mx-4 text-2xl font-semibold">TRENDING</div>
+        <div className="w-14 h-[2px] bg-black rounded-lg"></div>
+      </div>
+      <div className="italic text-center text-sm mb-8">
+        Top View in this week
+      </div>
+
+      <ProductList />
+    </div>
+  );
+};
+
+export default TrendingProducts;
