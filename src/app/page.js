cdc48f6069402a55fae2b1c7f487412cfb226fce
GokayDemirr <gokaydemirpc@gmail.com>
2025-02-12 03:57:22 +0300

    feat: Footer component implemented to main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,10 @@
+import BestSellingProducts from "@/components/BestSellingProducts";
+import Footer from "@/components/Footer";
 import CategoryCards from "@/components/Home/CategoryCards";
 import Carousel from "@/components/Home/Hero/Carousel";
+import News from "@/components/News";
 import TrendingProducts from "@/components/TrendingProducts";
+import TrustBadges from "@/components/TrustBadges";
 
 const Home = () => {
   return (
@@ -8,6 +12,10 @@ const Home = () => {
       <Carousel />
       <CategoryCards />
       <TrendingProducts />
+      <BestSellingProducts />
+      <News />
+      <TrustBadges />
+      <Footer />
     </div>
   );
 };
